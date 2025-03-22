9be26fbac96635732f62f58a7737c3a192a16793
Caroline Pham <carolinetpham@outlook.com>
2025-03-22 14:16:36 -0400

    adding starter home page text

---
+++ src/app/components/HomePage.tsx
@@ -0,0 +1,14 @@
+const HomePage = () => {
+  return (
+    <div id="home">
+      <div className="flex justify-between items-end w-full p-8 mt-6">
+        <h1 className="font-bold text-5xl">iHusky</h1>
+        <h1 className="font-bold text-xl">
+          Northeastern's iOS Development Club.
+        </h1>
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
