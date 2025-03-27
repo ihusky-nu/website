885b5f26ec7f141a7fcde055eadaf8e800742f1b
Caroline Pham <carolinetpham@outlook.com>
2025-03-27 19:38:13 -0400

    editing config

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
   output: "export",
-  trailingSlash: true, 
+  trailingSlash: true,
+  images: {
+    unoptimized: true, // Required for static export in Next.js on Netlify
+  },
 };
 
 export default nextConfig;
